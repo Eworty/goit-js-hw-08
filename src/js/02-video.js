56172b3e7266ff26a779d34bab03afb9ebf00bbd
Pasha Piankovskyi <ghostangel21042017@gmail.com>
2023-02-18 16:58:04 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const currentTime = function (data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+  const onPlay = player.on('timeupdate', throttle(currentTime, 1000));
+
+  let time = localStorage.getItem('videoplayer-current-time');
+  console.log(time);
+};
+
+player.on('timeupdate', currentTime);
+
+player
+  .setCurrentTime(localStorage.getItem(`videoplayer-current-time`))
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
